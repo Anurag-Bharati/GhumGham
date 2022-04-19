fd2195d1d34e020c05f935847efdc9b5bdc64735
Anurag-Bharati <anuragbharati26@gmail.com>
2022-04-19 16:08:49 +0545

    Configured settings.py | Added dummy test 🧪

--- GhumGham/settings.py
+++ GhumGham/settings.py
@@ -126,6 +126,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static/')]
+
+MEDIA_URL = 'images/uploads/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images/uploads')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
