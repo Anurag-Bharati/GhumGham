1eba130f1a7940310fb3ac0bc7f9920a53a0174f
rochaktamang <78839682+rochaktamang@users.noreply.github.com>
2022-05-23 12:49:34 +0545

    Added api end point for home page

--- home/views.py
+++ home/views.py
@@ -2,3 +2,12 @@ from django.shortcuts import render
 
 # Create your views here.
 
+
+def homepage(request):
+  if request.method == 'GET':
+    return render(request, 'index.html')
+    
+  elif request.method == 'POST':
+    return render(request, 'index.html')
+
+ 
