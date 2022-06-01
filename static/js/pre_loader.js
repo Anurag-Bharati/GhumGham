ce1b2bc91c839e2822a25d3d29599b6645286ffb
Anurag-Bharati <anuragbharati26@gmail.com>
2022-06-01 17:00:48 +0545

    Added GhumGham pre-loader w/ 1s delay | Did Animation ✨ of the pre-loader | Added Search section, Package section and filter options of explore.html

--- static/js/pre_loader.js
+++ static/js/pre_loader.js
@@ -1,4 +1,6 @@
-$(window).on("load",function(){
+$(window).on("load",async function () {
     /*----- Preloader -----*/
+    await new Promise(r => setTimeout(r, 1000));
     $(".preloader").fadeOut("slow");
+    // $(".preloader").slideUp(1000).fadeOut("slow");
 });
