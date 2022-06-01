27dacded12b1ef59e6fddfe7367065407d63cd76
Anurag-Bharati <anuragbharati26@gmail.com>
2022-06-01 17:10:46 +0545

    tweaked message and pre-loader parameters

--- static/js/pre_loader.js
+++ static/js/pre_loader.js
@@ -1,6 +1,6 @@
 $(window).on("load",async function () {
     /*----- Preloader -----*/
-    await new Promise(r => setTimeout(r, 1000));
-    $(".preloader").fadeOut("slow");
+    await new Promise(r => setTimeout(r, 500));
+    $(".preloader").fadeOut("fast");
     // $(".preloader").slideUp(1000).fadeOut("slow");
 });
