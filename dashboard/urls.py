f0bc42e7527c9c17eefbb38d2df6d9c4b6314629
prasesh729 <praseshdangol@gmail.com>
2022-06-09 15:57:37 +0545

    Finished the backend work of change password

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.Dashboard.as_view(), name="dashboard"),
     path('delete-log/<int:identity>', views.delete_log, name="delete-log"),
     path('ban-user/<int:identity>', views.ban_unban_user, name="ban-unban-user"),
+    path('change-pass/', views.change_pass, name="change-pass"),
 ]
